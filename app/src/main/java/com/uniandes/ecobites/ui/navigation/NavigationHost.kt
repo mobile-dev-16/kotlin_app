fe5188ac8b338e3ea77ad5f53d0493832872e596
Mario Alejandro Ruiz <56364082+ma-r-s@users.noreply.github.com>
2024-10-02 23:35:02 -0500

    Closes #10 (#24)

--- app/src/main/java/com/uniandes/ecobites/ui/navigation/NavigationHost.kt
+++ app/src/main/java/com/uniandes/ecobites/ui/navigation/NavigationHost.kt
@@ -1,10 +1,10 @@
 package com.uniandes.ecobites.ui.navigation
 
 import androidx.compose.runtime.Composable
+import com.uniandes.ecobites.ui.screens.ProfileScreen
 import com.uniandes.ecobites.ui.screens.CartScreen
 import com.uniandes.ecobites.ui.screens.home.HomeScreen
 import com.uniandes.ecobites.ui.screens.OrdersScreen
-import com.uniandes.ecobites.ui.screens.ProfileScreen
 
 @Composable
 fun NavigationHost(selectedTab: Int) {
